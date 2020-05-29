95bdea36263faac7913dac1f8f6405ba0bf99339
amu3dev <amu3dev@gmail.com>
2020-05-29 22:15:56 +0200

    apply ...rest in <input

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,18 +1,9 @@
 import React from "react";
-const Input = ({ type, name, label, value, error, onChange }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        name={name}
-        value={value}
-        onChange={onChange}
-        autoFocus
-        //ref={this.username}
-        id={name}
-        type={type}
-        className="form-control"
-      />
+      <input {...rest} name={name} id={name} className="form-control" />
       {/* div.alert.alert-danger */}
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
