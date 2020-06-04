b9c187f9e7a094b744142c9e3eb9b64d04feab62
amu3dev <amu3dev@gmail.com>
2020-06-04 22:19:48 +0200

    init MovieForm

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,17 +1,48 @@
 import React from "react";
+import Form from "./common/form";
+import Joi from "joi-browser";
+import { getGenres } from "../services/fakeGenreService";
 
-const MovieForm = ({ match, history }) => {
-  return (
-    <div>
-      <h1> Movie {match.params.id}</h1>
-      <button
-        className="btn btn-primary"
-        onClick={() => history.push("/movies")}
-      >
-        Save
-      </button>
-    </div>
-  );
-};
+class MovieForm extends Form {
+  state = {
+    data: { title: "", genreId: "", numberInStock: "", rate: "" },
+    genres: [],
+    errors: {},
+  };
+
+  schema = {
+    title: Joi.string().required().label("Title"),
+    genreId: Joi.string().required().label("Genre"),
+    numberInStock: Joi.number()
+      .min(0)
+      .max(100)
+      .required()
+      .label("Number in Stock"),
+    rate: Joi.number().min(0).max(10).required().label("Rate"),
+  };
+  componentDidMount() {
+    const genres = getGenres();
+    this.setState({ genres });
+  }
+
+  doSubmit = () => {
+    // call the server --
+    console.log("movieForm-- submitted");
+  };
+  render() {
+    return (
+      <div>
+        <h1>Movie Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("title", "Title")}
+          {this.renderSelect("genreId", "Genre", this.state.genres)}
+          {this.renderInput("numberInStock", "Number in Stock", "number")}
+          {this.renderInput("rate", "Rate")}
+          {this.renderButton("Save")}
+        </form>
+      </div>
+    );
+  }
+}
 
 export default MovieForm;
