c853a4338e1800cae2072f59203b17daa5252dc6
amu3dev <amu3dev@gmail.com>
2020-06-09 00:21:24 +0200

    comment console.log

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -6,11 +6,11 @@ const Pagination = (props) => {
   const { itemsCount, pageSize, onPageChange, currentPage } = props;
   const pagesCount = Math.ceil(itemsCount / pageSize);
   const pages = _.range(1, pagesCount + 1);
-  console.log("pages=>", pages);
+  /*   console.log("pages=>", pages);
   console.log("pageCount=>", pagesCount);
   console.log("itemCount=>", itemsCount);
   console.log("pageSize=>", pageSize);
-  console.log("currentPage=>", currentPage);
+  console.log("currentPage=>", currentPage); */
 
   return (
     <nav aria-label="Page navigation example">
